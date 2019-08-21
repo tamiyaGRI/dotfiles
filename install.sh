e270027886b00f4a637e98ae1cab5bd77d0e18cf
Haretaro Takahashi <takahashi.seitaro@gri.jp>
2019-08-21 09:01:58 +0900

    git 関係のファイルを wget で持ってくるようにした

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
